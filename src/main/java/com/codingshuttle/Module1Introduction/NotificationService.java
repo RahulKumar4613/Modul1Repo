0fa4fe31e4b80f3010279777ffa581207d89afb7
Rahul <rahulguptarfj2@gail.com>
2026-02-16 05:41:02 +0530

    adding new file in dev branch

---
+++ src/main/java/com/codingshuttle/Module1Introduction/NotificationService.java
@@ -0,0 +1,9 @@
+package com.codingshuttle.Module1Introduction;
+
+import org.springframework.stereotype.Component;
+
+
+public interface NotificationService {
+
+      void sendMessage(String msg);
+}
