7dbe396760db6feb889d18f9eaccbaa18acd0c1b
Rahul <rahulguptarfj2@gail.com>
2026-02-19 06:22:45 +0530

    Final Comit2

--- src/main/java/com/codingshuttle/Module1Introduction/impl/EmailNotification.java
+++ src/main/java/com/codingshuttle/Module1Introduction/impl/EmailNotification.java
@@ -1,11 +1,13 @@
 package com.codingshuttle.Module1Introduction.impl;
 
 import com.codingshuttle.Module1Introduction.NotificationService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
+//@Primary
+@ConditionalOnProperty(name = "Notification.type",havingValue="Email")
 public class EmailNotification implements NotificationService {
     @Override
     public void sendMessage(String msg) {
--- src/main/java/com/codingshuttle/Module1Introduction/impl/SmsNotification.java
+++ src/main/java/com/codingshuttle/Module1Introduction/impl/SmsNotification.java
@@ -1,9 +1,11 @@
 package com.codingshuttle.Module1Introduction.impl;
 
 import com.codingshuttle.Module1Introduction.NotificationService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "Notification.type",havingValue="sms")
 public class SmsNotification implements NotificationService {
     @Override
     public void sendMessage(String msg) {
